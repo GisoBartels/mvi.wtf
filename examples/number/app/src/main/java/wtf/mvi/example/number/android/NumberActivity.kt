5424f2342c8c6da58b6c5e8a09163c5a9031835b
Giso Bartels <wtf@giso-bartels.de>
2020-02-20 21:42:57 +0100

    update Jetpack Compose to dev05

--- examples/number/app/src/main/java/wtf/mvi/example/number/android/NumberActivity.kt
+++ examples/number/app/src/main/java/wtf/mvi/example/number/android/NumberActivity.kt
@@ -2,18 +2,18 @@ package wtf.mvi.example.number.android
 
 import android.app.Activity
 import android.os.Bundle
-import androidx.compose.*
+import androidx.compose.Composable
+import androidx.compose.onCommit
+import androidx.compose.remember
+import androidx.compose.state
 import androidx.ui.core.Text
-import androidx.ui.core.dp
 import androidx.ui.core.setContent
-import androidx.ui.core.sp
-import androidx.ui.layout.Center
-import androidx.ui.layout.CrossAxisAlignment
-import androidx.ui.layout.Row
-import androidx.ui.layout.WidthSpacer
+import androidx.ui.layout.*
 import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
 import androidx.ui.text.TextStyle
+import androidx.ui.unit.dp
+import androidx.ui.unit.sp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
@@ -34,34 +34,39 @@ class NumberActivity : Activity(), NumberView, CoroutineScope by MainScope() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val viewState = +collect(numberPresenter.renderFlow)
+            val viewState = collect(numberPresenter.renderFlow)
             MaterialTheme {
                 Center {
-                    Row(crossAxisAlignment = CrossAxisAlignment.Center) {
-                        Button("-", onClick = { numberPresenter.onIntent(MinusIntent) })
-                        WidthSpacer(width = 16.dp)
+                    Row(arrangement = Arrangement.Center) {
+                        Button(onClick = { numberPresenter.onIntent(MinusIntent) }) {
+                            Text("-")
+                        }
+                        Spacer(modifier = LayoutWidth(16.dp))
                         Text(
                             text = viewState?.number?.toString() ?: "",
                             style = TextStyle(fontSize = 24.sp)
                         )
-                        WidthSpacer(width = 16.dp)
-                        Button("+", onClick = { numberPresenter.onIntent(PlusIntent) })
+                        Spacer(modifier = LayoutWidth(16.dp))
+                        Button(onClick = { numberPresenter.onIntent(PlusIntent) }) {
+                            Text("+")
+                        }
                     }
                 }
             }
         }
     }
 
-    private fun <T> CoroutineScope.collect(flow: Flow<T>) = effectOf<T?> {
-        val state = +state<T?> { null }
-        val observer = +memo { { newValue: T -> state.value = newValue } }
+    @Composable
+    private fun <T> CoroutineScope.collect(flow: Flow<T>): T? {
+        val state = state<T?> { null }
+        val observer = remember { { newValue: T -> state.value = newValue } }
 
-        +onCommit(flow) {
+        onCommit(flow) {
             val job = launch { flow.collect { observer(it) } }
             onDispose { job.cancel() }
         }
 
-        state.value
+        return state.value
     }
 
     override fun onDestroy() {
